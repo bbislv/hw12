c9235e985f2b26ed362e0ad955123d9c7306609c
ekaterinabobyleva <bbislv12@gmail.com>
2024-12-01 16:51:54 +0300

    hw12-13

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Author author1 = new Author("Lev", "Tolstoy");
+
+        Book book1 = new Book("War and Peace", 1867, author1);
+
+        System.out.println(aut hor1);
+
+        System.out.println(book1);
     }
 }
